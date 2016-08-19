156dfee3958dbb3a45f108930ac08f8a23119844
yejin choi <helloppo.choi@gmail.com>
2016-08-19 21:12:01 +0900

    made the toolbar. But it was not saved without committing. It's ok. anyway i tryed to delete toolbar. because we don't need toolbar at our UI. and i mede recyclerView. this is list of favorite routes.

--- DrivingPartner/app/src/main/java/com/devfactory/drivingpartner/MainActivity.java
+++ DrivingPartner/app/src/main/java/com/devfactory/drivingpartner/MainActivity.java
@@ -3,10 +3,19 @@ package com.devfactory.drivingpartner;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 public class MainActivity extends Activity {
+
+    //예제 리스트 (지울것)
+    ArrayList<String> arrayList1;
+
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,27 +25,45 @@ public class MainActivity extends Activity {
         // 스플레시를 실행한다.
         // 로그인인 페이지 실행도 나중에 여기에 구현할 예정이다.
         startActivity(new Intent(this, SplashActivity.class));
+
+        initData();
+        initUi();
+    }
+
+    /**
+     * initData()
+     * 메인 엑티비티에서 사용할 Data를 초기화 하는 메소드
+     */
+    private void initData() {
+        arrayList1 = new ArrayList<>();
+        arrayList1.add("인천대공원 > 인하대학교 > 월미도");
+        arrayList1.add("관양1동주민센터");
+        arrayList1.add("왕십리 이마트 지상주차장");
+        arrayList1.add("한양대학교");
+    }
+
+    /**
+     * initUi()
+     * 메인 엑티비티에서 사용할 widget을 초기화 선언하는 메소드
+     */
+    private void initUi() {
+
+        // Recyclerview 선언
+        // 최근 경로나, 자주가는 경로를 보여주는 리스트 이다.
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_main);
+        mRecyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+
+        MainRecyclerAdapter adapter = new MainRecyclerAdapter(arrayList1);
+        adapter.notifyDataSetChanged();
+        mRecyclerView.setAdapter(adapter);
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onResume() {
+        super.onResume();
     }
 }
